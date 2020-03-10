7eaf1bbfc2c61b43884f57f2b7cb23fe40c5bc53
Stas Rirak <stasrira@yahoo.com>
2020-03-10 11:08:33 -0400

    Adjusts help message with description for the newly added 4th paramter.

--- process_requests.sh
+++ process_requests.sh
@@ -20,6 +20,7 @@ _HELP="\n$_VERSION
 		\n\t\t\t- First argument provides URL to the data to be downloaded.
 		\n\t\t\t- Second argument specifies path to the location where downloaded data will be saved.
 		\n\t\t\t- Third argument specifies the folder name that will be created in the given path; all downloaded data will be save in this folder.
+		\n\t\t\t- Fourth (optional) argument specifies the type of object being downloaded. Expected values: 1) dir (default value), 2) file.
 		\n\t[-s: pattern that will be used to search for Download Request files in the given folder]
 		\n\t[-m: command that will be used to perfomr the download/copy process. If not provided, the data source for each entry in the request file will be analyzed to select the appropriate command.
 		\n\t\t- expected values are 'wget' or 'cp'].
